44f3fcbaeb8fcc228ed252e8a99c61405a39f2fb
akiran1sei <nakamori.1224@gmail.com>
2024-08-26 15:01:54 +0900

    コーディング準備

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,17 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import styles from "../styles/page.module.css";
+// import Link from "next/link";
 
 const home = () => {
   return (
     <main className={styles.main}>
       <div className={styles.main_wrapper}>
-        <div className={styles.home_contents}>Home</div>
+        <div className={styles.home_contents}>
+          <div className={styles.home_notebook}>
+            <div className={styles.home_notebook_spine}></div>
+            <div className={styles.home_notebook_cover}></div>
+          </div>
+        </div>
       </div>
     </main>
   );
