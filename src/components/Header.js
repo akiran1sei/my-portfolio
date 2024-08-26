063d7e78cd0890bf43a40cb6eb3e8843115794a6
akiran1sei <nakamori.1224@gmail.com>
2024-08-26 16:37:33 +0900

    コンポーネントファイルであるheaderを作成中

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,65 @@
+import Link from "next/link";
+import styles from "../styles/page.module.css";
 const Header = () => {
   return (
-    <ul>
-      <li>1</li>
-      <li>2</li>
-    </ul>
+    <header className={styles.header}>
+      <div className={styles.header_wrapper}>
+        <nav className={styles.header_nav}>
+          <ul className={styles.header_nav_list}>
+            <li className={styles.header_nav_item}>
+              <Link
+                className={styles.header_nav_link}
+                href={"http://localhost:3000/pages/about"}
+                scroll={false}
+                passHref
+              >
+                About
+              </Link>
+            </li>
+            <li className={styles.header_nav_item}>
+              <Link
+                className={styles.header_nav_link}
+                href={"http://localhost:3000/pages/works"}
+                scroll={false}
+                passHref
+              >
+                Works
+              </Link>
+            </li>
+            <li className={styles.header_nav_item}>
+              <Link
+                className={styles.header_nav_link}
+                href={"http://localhost:3000/pages/blog"}
+                scroll={false}
+                passHref
+              >
+                Blog
+              </Link>
+            </li>
+            <li className={styles.header_nav_item}>
+              <Link
+                className={styles.header_nav_link}
+                href={"http://localhost:3000/pages/contact"}
+                scroll={false}
+                passHref
+              >
+                Contact
+              </Link>
+            </li>
+            {/* <li className={styles.header_nav_item}>
+              <Link
+                className={styles.header_nav_link}
+                href={"http://localhost:3000/pages/preview"}
+                scroll={false}
+                passHref
+              >
+                Preview
+              </Link>
+            </li> */}
+          </ul>
+        </nav>
+      </div>
+    </header>
   );
 };
 export default Header;
