d0d64a795f8e5cee1f7b4cb9a1365fc0f0a5309a
akiran1sei <nakamori.1224@gmail.com>
2024-08-27 16:42:22 +0900

    💄 レイアウトコード編集　

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
 import "../styles/globals.css";
-import styles from "../styles/page.module.css";
+import styles from "@/styles/page.module.css";
 import "normalize.css";
 const inter = Inter({ subsets: ["latin"] });
 
