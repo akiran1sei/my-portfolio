bcf749f1aeb33f3072da213fd6a634a08aeefc34
akiran1sei <nakamori.1224@gmail.com>
2024-09-03 12:11:32 +0900

    🎨 blogページ作成作業

--- src/app/pages/blog/page.jsx
+++ src/app/pages/blog/page.jsx
@@ -1,5 +1,29 @@
+"use client";
 import styles from "@/styles/page.module.css";
-const blog = () => {
-  return <div className={styles.contents}></div>;
+
+import { UpButton } from "@/components/Buttons/UpButton";
+import { useState, useEffect } from "react";
+
+const Blog = () => {
+  const [isActive, setIsActive] = useState(false);
+
+  useEffect(() => {
+    // ページ読み込み時に実行される処理
+    setIsActive(true);
+  }, []); // 空の配列を渡すことで、マウント時に一度だけ実行される
+  return (
+    <div className={styles.contents}>
+      <UpButton />
+
+      <section
+        className={`${styles.blog_section} ${isActive ? styles.active : " "}`}
+      >
+        <h2 className={styles.page_title}>Blog</h2>
+        <div className={styles.blog_contents}>
+          <div className={styles.blog_card}></div>
+        </div>
+      </section>
+    </div>
+  );
 };
-export default blog;
+export default Blog;
