0bdd07a1aacd6d8016c891d0fdfc73d32d04c6ec
akiran1sei <nakamori.1224@gmail.com>
2024-09-06 12:48:38 +0900

    🎨 ボタン編集

--- src/components/Buttons/BackButton.js
+++ src/components/Buttons/BackButton.js
@@ -1,7 +1,19 @@
+"use client";
 import styles from "@/styles/page.module.css";
+import { useRouter } from "next/navigation";
 export function BackButton() {
+  const router = useRouter();
+  const handleBackButton = (e) => {
+    e.preventDefault();
+
+    return router.back();
+  };
   return (
-    <button type="button" className={styles.backButton}>
+    <button
+      type="button"
+      className={styles.backButton}
+      onClick={handleBackButton}
+    >
       戻る
     </button>
   );
