30e1d3b47be72cccd36cb6f5e48f7568a46bf5a1
akiran1sei <nakamori.1224@gmail.com>
2024-09-26 16:44:59 +0900

    🚧 オーナー側のレイアウトデザイン作成中

---
+++ src/components/DachboardHeader.js
@@ -0,0 +1,61 @@
+import Link from "next/link";
+import React from "react";
+import styles from "@/styles/page.module.css";
+
+export function DashboardHeader() {
+  return (
+    <header className={styles.dashboard_header} id="dashboard_header">
+      <div className={styles.dashboard_header_wrapper}>
+        <nav className={styles.dashboard_header_nav}>
+          <ul className={styles.dashboard_header_nav_list}>
+            <li className={styles.dashboard_header_nav_item}>
+              <Link
+                className={styles.dashboard_header_nav_link}
+                href={"/pages/dashboard/post"}
+                passHref
+              >
+                Post
+              </Link>
+            </li>
+            <li className={styles.dashboard_header_nav_item}>
+              <Link
+                className={styles.dashboard_header_nav_link}
+                href={"/pages/dashboard/edit"}
+                passHref
+              >
+                Edit
+              </Link>
+            </li>
+            <li className={styles.dashboard_header_nav_item}>
+              <Link
+                className={styles.dashboard_header_nav_link}
+                href={"/pages/dashboard/delete"}
+                passHref
+              >
+                Delete
+              </Link>
+            </li>
+            <li className={styles.dashboard_header_nav_item}>
+              <Link
+                className={styles.dashboard_header_nav_link}
+                href={"/pages/dashboard/update"}
+                passHref
+              >
+                Update
+              </Link>
+            </li>
+            <li className={styles.dashboard_header_nav_item}>
+              <Link
+                className={styles.dashboard_header_nav_link}
+                href={"/pages/dashboard/preview"}
+                passHref
+              >
+                Preview
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+}
