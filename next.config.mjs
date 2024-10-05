c72ae7ce11369da157732bef20372c6482742566
akiran1sei <nakamori.1224@gmail.com>
2024-10-05 10:45:00 +0900

    元に戻した

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "**.public.blob.vercel-storage.com",
-      },
-    ],
-  },
-  // 他の既存の設定がある場合は、ここに追加します
-};
+const nextConfig = {};
 
 export default nextConfig;
