0a3dd7cf04b1d9f415eac4ecf93afa2297a58a19
akiran1sei <nakamori.1224@gmail.com>
2024-10-07 12:14:42 +0900

    🎨 再編集

--- src/app/pages/dashboard/upload/page.jsx
+++ src/app/pages/dashboard/upload/page.jsx
@@ -117,7 +117,7 @@ export default function ImageUploader() {
           </div>
         </SignedIn>
         <SignedOut>
-          <p>サインインしてください。</p>
+          <p className={styles.signOut_text}>サインインしてください。</p>
         </SignedOut>
       </section>
     </>
