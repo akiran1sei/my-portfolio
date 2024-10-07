548217932b03bef420a0dede30bbb6408b05ef63
akiran1sei <nakamori.1224@gmail.com>
2024-10-07 09:20:07 +0900

    images設定

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["lpxfluplifv7xzbr.public.blob.vercel-storage.com"],
+  },
+};
 
 export default nextConfig;
