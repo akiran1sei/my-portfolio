705ecb9ca623c35be7c6a1c9a14d49a21c9b8534
akiran1sei <nakamori.1224@gmail.com>
2025-07-10 09:27:49 +0900

    codes スキーマを設定

--- src/utils/schemaModels.js
+++ src/utils/schemaModels.js
@@ -1,16 +1,6 @@
 import mongoose from "mongoose";
 const Schema = mongoose.Schema;
-const BlogSchema = new Schema({
-  postTitle: String,
-  postMessage: String,
-  postDate: String,
-  postImage: String,
-  postImageAlt: String,
-  postDraft: Boolean,
-  createdAt: Date,
-  updatedAt: Date,
-});
-const DraftSchema = new Schema(
+const BlogSchema = new Schema(
   {
     postTitle: String,
     postMessage: String,
@@ -18,9 +8,14 @@ const DraftSchema = new Schema(
     postImage: String,
     postImageAlt: String,
     postDraft: Boolean,
+    createdAt: Date,
+    updatedAt: Date,
   },
   { timestamps: true }
 );
+const CodesSchema = new Schema({
+  code: String,
+});
 const ImageSchema = new Schema({
   name: String,
   url: String,
@@ -29,6 +24,6 @@ const ImageSchema = new Schema({
 export const BlogModel =
   mongoose.models.Blog || mongoose.model("Blog", BlogSchema);
 export const DraftModel =
-  mongoose.models.Draft || mongoose.model("Draft", DraftSchema);
+  mongoose.models.Draft || mongoose.model("Draft", CodesSchema);
 export const ImageModel =
   mongoose.models.Image || mongoose.model("Image", ImageSchema);
